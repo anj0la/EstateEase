494f4a324afaa40b0b3687a9f238c9d26d7e3d52
anj0la <anjola.aina@gmail.com>
2025-01-07 20:35:40 -0700

    Added database connector and base query class for SQLite.

---
+++ EstateEase/Database/BaseQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstateEase.Database;
+
+namespace EstateEase.Database
+{
+    public abstract class BaseQuery(DatabaseConnector databaseConnection)
+    {
+        protected readonly DatabaseConnector _databaseConnection = databaseConnection;
+
+    }
+}
---
+++ EstateEase/Database/DatabaseConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace EstateEase.Database
+{
+    public class DatabaseConnector(string connectionString)
+    {
+        private readonly string _connectionString = connectionString;
+    
+
+    public SQLiteConnection GetConnection()
+        {
+            var connection = new SQLiteConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+
+}
