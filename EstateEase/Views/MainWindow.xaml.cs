e778922c8d9cdde70d95a609302307c48597ec34
anj0la <anjola.aina@gmail.com>
2025-01-08 18:56:25 -0700

    Added SQLite database to application, and updated .gitignore to not track the db.

--- EstateEase/Views/MainWindow.xaml.cs
+++ EstateEase/Views/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using EstateEase.Database;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,7 +33,10 @@ namespace EstateEase
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            
+        myButton.Content = "Clicked!";
+           
         }
+
     }
 }
